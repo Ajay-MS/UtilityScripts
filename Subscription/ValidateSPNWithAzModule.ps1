5002bd903c5cc2409bf8d82aac4c36f7fca2583d
Ajay Kumar Yadav <ajya@microsoft.com>
2019-05-14 17:10:04 +0530

    Create ValidateSPNWithAzModule.ps1

---
+++ Subscription/ValidateSPNWithAzModule.ps1
@@ -0,0 +1,12 @@
+$servicePrincipalId = "<ServicePrincipalID>"
+$servicePrincipalKey = "<servicePrincipalKey>"
+$tenantId = "<tenantId>"
+$azureSubscriptionId = "<azureSubscriptionId>" 
+
+$securePassword = ConvertTo-SecureString $servicePrincipalKey -AsPlainText -Force
+$psCredential = New-Object System.Management.Automation.PSCredential ($servicePrincipalId, $securePassword)
+
+Write-Verbose "Connect-AzAccount Started" -Verbose
+$azureAccount = Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $psCredential -Environment AzureCloud
+$azureAccount
+Write-Verbose "Add-AzureRMAccount Completed" -Verbose
