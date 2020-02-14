a082d7d6f44d3b8c6b3c402d5b40fbae5c641358
Ajay Kumar Yadav <ajya@microsoft.com>
2020-02-14 15:12:39 +0530

    Update replica.sh

--- replica.sh
+++ replica.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Configuring GHES replica on $1"
+echo "Configuring GHES replica"
 echo y | ghe-repl-setup $1 --force --verbose
 echo "GHES replica setup command executed"
 ghe-repl-start
